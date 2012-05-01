59684149c18ac5e2f391ecd4291d39f82afbf7cd
darkf <lw9k123@gmail.com>
2012-05-01 17:44:51 -0500

    meh

--- op.c
+++ op.c
@@ -1,7 +1,25 @@
-#include "op.h"
 #include <stdlib.h>
+#include "op.h"
 
-const char *op_tostr(signed int op)
+static const char* opnames[] = {
+	"OP_NOP",
+	"OP_ZERO",
+	"OP_MOV8", "OP_MOV16", "OP_MOV32",
+	"OP_PUSH", "OP_POP",
+	"OP_SHL", "OP_SHR",
+	"OP_AND", "OP_OR", "OP_XOR", "OP_NOT",
+	"OP_ADD", "OP_SUB", "OP_MUL", "OP_DIV", "OP_MOD",
+	"OP_INT",
+	"OP_CMP",
+	"OP_B", "OP_BZ", "OP_BNZ", "OP_BLT", "OP_BGT", "OP_BE", "OP_BNE",
+	"OP_HALT",
+
+	"OP_COUNT_MINUS_ONE",
+	"OP_COUNT"
+};
+
+
+const char *op_tostr(int op)
 {
 	if(op > OP_COUNT) return NULL;
 	return opnames[op];
--- op.h
+++ op.h
@@ -1,3 +1,6 @@
+#ifndef OP_H_INCLUDED
+#define OP_H_INCLUDED
+
 enum OP {
 	OP_NOP = 0,
 	OP_ZERO,
@@ -15,23 +18,6 @@ enum OP {
 	OP_COUNT
 };
 
-char* opnames[] = {
-	"OP_NOP",
-	"OP_ZERO",
-	"OP_MOV8", "OP_MOV16", "OP_MOV32",
-	"OP_PUSH", "OP_POP",
-	"OP_SHL", "OP_SHR",
-	"OP_AND", "OP_OR", "OP_XOR", "OP_NOT",
-	"OP_ADD", "OP_SUB", "OP_MUL", "OP_DIV", "OP_MOD",
-	"OP_INT",
-	"OP_CMP",
-	"OP_B", "OP_BZ", "OP_BNZ", "OP_BLT", "OP_BGT", "OP_BE", "OP_BNE",
-	"OP_HALT",
-
-	"OP_COUNT_MINUS_ONE",
-	"OP_COUNT"
-};
-
 /* operand type codes */
 enum {
         REGISTER = 0,
@@ -41,3 +27,5 @@ enum {
 };
 
 const char *op_tostr(int op);
+
+#endif
